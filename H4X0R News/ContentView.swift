899912eda8b566a1726b3e9826680977c8682a63
rexam14 <amerigo.mancino@gmail.com>
2019-10-13 15:12:30 +0200

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Amerigo Mancino on 13/10/2019.
+//  Copyright © 2019 Amerigo Mancino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
